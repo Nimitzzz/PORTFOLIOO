1059f978107c3c97e4e9eaa0982a409a3041ded6
NIMIT <nimitj2003@gmail.com>
2023-06-12 01:21:40 +0530

    final

---
+++ main.js
@@ -0,0 +1,185 @@
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+(function () {
+    // VARIABLES
+    const timeline = document.querySelector(".timeline ol"),
+      elH = document.querySelectorAll(".timeline li > div"),
+      arrows = document.querySelectorAll(".timeline .arrows .arrow"),
+      arrowPrev = document.querySelector(".timeline .arrows .arrow__prev"),
+      arrowNext = document.querySelector(".timeline .arrows .arrow__next"),
+      firstItem = document.querySelector(".timeline li:first-child"),
+      lastItem = document.querySelector(".timeline li:last-child"),
+      xScrolling = 280,
+      disabledClass = "disabled";
+  
+    // START
+    window.addEventListener("load", init);
+  
+    function init() {
+      setEqualHeights(elH);
+      animateTl(xScrolling, arrows, timeline);
+      setSwipeFn(timeline, arrowPrev, arrowNext);
+      setKeyboardFn(arrowPrev, arrowNext);
+    }
+  
+    // SET EQUAL HEIGHTS
+    function setEqualHeights(el) {
+      let counter = 0;
+      for (let i = 0; i < el.length; i++) {
+        const singleHeight = el[i].offsetHeight;
+  
+        if (counter < singleHeight) {
+          counter = singleHeight;
+        }
+      }
+  
+      for (let i = 0; i < el.length; i++) {
+        el[i].style.height = `${counter}px`;
+      }
+    }
+  
+    // CHECK IF AN ELEMENT IS IN VIEWPORT
+    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    function isElementInViewport(el) {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  
+    // SET STATE OF PREV/NEXT ARROWS
+    function setBtnState(el, flag = true) {
+      if (flag) {
+        el.classList.add(disabledClass);
+      } else {
+        if (el.classList.contains(disabledClass)) {
+          el.classList.remove(disabledClass);
+        }
+        el.disabled = false;
+      }
+    }
+  
+    // ANIMATE TIMELINE
+    function animateTl(scrolling, el, tl) {
+      let counter = 0;
+      for (let i = 0; i < el.length; i++) {
+        el[i].addEventListener("click", function () {
+          if (!arrowPrev.disabled) {
+            arrowPrev.disabled = true;
+          }
+          if (!arrowNext.disabled) {
+            arrowNext.disabled = true;
+          }
+          const sign = this.classList.contains("arrow__prev") ? "" : "-";
+          if (counter === 0) {
+            tl.style.transform = `translateX(-${scrolling}px)`;
+          } else {
+            const tlStyle = getComputedStyle(tl);
+            // add more browser prefixes if needed here
+            const tlTransform =
+              tlStyle.getPropertyValue("-webkit-transform") ||
+              tlStyle.getPropertyValue("transform");
+            const values =
+              parseInt(tlTransform.split(",")[4]) +
+              parseInt(`${sign}${scrolling}`);
+            tl.style.transform = `translateX(${values}px)`;
+          }
+  
+          setTimeout(() => {
+            isElementInViewport(firstItem)
+              ? setBtnState(arrowPrev)
+              : setBtnState(arrowPrev, false);
+            isElementInViewport(lastItem)
+              ? setBtnState(arrowNext)
+              : setBtnState(arrowNext, false);
+          }, 1100);
+  
+          counter++;
+        });
+      }
+    }
+  
+    // ADD SWIPE SUPPORT FOR TOUCH DEVICES
+    function setSwipeFn(tl, prev, next) {
+      const hammer = new Hammer(tl);
+      hammer.on("swipeleft", () => next.click());
+      hammer.on("swiperight", () => prev.click());
+    }
+  
+    // ADD BASIC KEYBOARD FUNCTIONALITY
+    function setKeyboardFn(prev, next) {
+      document.addEventListener("keydown", (e) => {
+        if (e.which === 37 || e.which === 39) {
+          const timelineOfTop = timeline.offsetTop;
+          const y = window.pageYOffset;
+          if (timelineOfTop !== y) {
+            window.scrollTo(0, timelineOfTop);
+          }
+          if (e.which === 37) {
+            prev.click();
+          } else if (e.which === 39) {
+            next.click();
+          }
+        }
+      });
+    }
+  })();
+  
